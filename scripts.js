060313df06aa83fbab85dafec4d98bffdfc34711
Measure Strategy <87092316+measurestrategy@users.noreply.github.com>
2024-05-10 15:21:39 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
 (function (){
-	console.log('script added');
+  console.log('script added');
+  
+  // OT
+  var script = document.createElement('script');
+  script.src = "https://cdn.cookielaw.org/scripttemplates/otSDKStub.js";
+  script.setAttribute('data-domain-script', "74e36f57-e4c3-4d88-9277-0480c053f26e");
+  script.charset = "UTF-8";
+  script.setAttribute('data-dLayer-ignore', "true");
+  script.setAttribute('data-document-language', "true");
+  document.head.appendChild(script);
+  window.OptanonWrapper = function(){};
+  
 })();
